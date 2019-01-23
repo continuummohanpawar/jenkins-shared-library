e685f7c65b6370a2d356326c0d9d70fec59ef572
continuum-visharad-dhavle <visharad.dhavle@continuum.net>
2019-01-23 18:08:14 +0530

    facade added in vars 10

--- vars/facade.groovy
+++ vars/facade.groovy
@@ -5,33 +5,9 @@ class facade
 {
     def execute()
     {
-        def label = "maven-${UUID.randomUUID().toString()}"
-        podTemplate(label: label, yaml: """
-apiVersion: v1
-kind: Pod
-metadata:
-  labels:
-    some-label: some-label-value
-spec:
-  containers:
-  - name: maven
-    image: maven:3.3.9-jdk-8-alpine
-    command:
-    - cat
-    tty: true
-    env:
-    - name: CONTAINER_ENV_VAR
-      value: container-env-var-value
-"""
-    ) {
-
-            node(label) {
-                stage('Build a Maven project') {
-                    git 'https://github.com/jenkinsci/kubernetes-plugin.git'
-                    container('maven') {
-                        sh 'mvn -B clean package'
-                    }
-                }
+        node(master) {
+            stage('Build a Maven project') {
+                git 'https://github.com/jenkinsci/kubernetes-plugin.git'                    
             }
         }
     }
