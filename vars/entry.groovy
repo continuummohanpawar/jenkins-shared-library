7f9c6dc959fbd1b5aa08ef8cb341ec31c322fb63
continuum-visharad-dhavle <visharad.dhavle@continuum.net>
2019-01-25 18:41:17 +0530

    trying to invoke kubernetes 17

--- vars/entry.groovy
+++ vars/entry.groovy
@@ -2,7 +2,7 @@
 
 def call(body) {
     // evaluate the body block, and collect configuration into the object
-    println(config)
+    println(body)
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
