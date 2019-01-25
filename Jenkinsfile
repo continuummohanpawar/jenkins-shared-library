64af96513e7bdcc835ff65cf4252aee90c3adeeb
continuum-visharad-dhavle <visharad.dhavle@continuum.net>
2019-01-25 17:20:32 +0530

    trying to invoke kubernetes 1

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,5 +1,7 @@
 
-@Library("shared_lib")_
+@Library("shared_lib") _
 
-f = new facade()
-f.execute()
+//f = new facade()
+//f.execute()
+
+entry image: 'maven', message: 'This is Visharad'
---
+++ vars/entry.groovy
@@ -0,0 +1,20 @@
+#!/usr/bin/groovy
+
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    kubernetes.pod('buildpod')
+    .withNewContainer()
+        .withName('maven-container')
+        .withImage('maven')
+    .withPrivileged(true)            
+    .inside {     
+        sh "echo 'Image: ${config.image}'"
+        sh "echo 'Image: ${config.message}'"
+      }
+    }
+  }
