0ce0ae445fe8e89411e772a39d8940ae68b1ed40
continuum-visharad-dhavle <visharad.dhavle@continuum.net>
2019-01-25 19:17:25 +0530

    trying to invoke kubernetes 27

--- vars/entry.groovy
+++ vars/entry.groovy
@@ -15,10 +15,9 @@ def call(body) {
     //node{   
         //sh "echo 'Image: ${config.image}'"
         //sh "echo 'Image: ${config.message}'"
-        kubernetes.pod('buildpod')
-            .withNewContainer().withName('maven').withImage('maven:3.3.9-jdk-8-alpine').withCommand('cat').withTtyEnabled()
+        kubernetes.pod('buildpod').withImage('maven')
+            //.withNewContainer().withName('maven').withImage('maven:3.3.9-jdk-8-alpine').withCommand('cat').withTtyEnabled()
             .withPrivileged(true)
-            .withLabel('name','maven')
             .inside { 
                 sh "echo 'Image: ${config.image}'"
                 sh "echo 'Image: ${config.message}'"
