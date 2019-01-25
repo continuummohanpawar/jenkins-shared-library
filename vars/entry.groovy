50108f16d91ebde9012097648e4de1a5d7466216
continuum-visharad-dhavle <visharad.dhavle@continuum.net>
2019-01-25 19:23:33 +0530

    trying to invoke kubernetes 29

--- vars/entry.groovy
+++ vars/entry.groovy
@@ -6,21 +6,17 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()   
-    //kubernetes.pod('buildpod')
-    //.withNewContainer()
-    //    .withName('maven-container')
-    //    .withImage('maven')
-    //.withPrivileged(true)            
-    //.inside {  
-    //node{   
-        //sh "echo 'Image: ${config.image}'"
-        //sh "echo 'Image: ${config.message}'"
-        kubernetes.pod('buildpod').withImage('maven')
-            //.withNewContainer().withName('maven').withImage('maven:3.3.9-jdk-8-alpine').withCommand('cat').withTtyEnabled()            
-            .inside {
-                sh "echo 'Image: ${config.image}'"
-                sh "echo 'Image: ${config.message}'"
-            }        
-    //}
-    //}
+    
+    /* Working Code with Pod
+    kubernetes.pod('buildpod').withImage('maven')
+        .inside {
+            sh "echo 'Image: ${config.image}'"
+            sh "echo 'Image: ${config.message}'"
+        }        
+    */
+    /*  */
+    kubernetes.pod('buildpod')
+    .withNewContainer().withName('maven').withImage('maven').inside {
+        sh 'Hi, I am inside a container'
+    }
 }
