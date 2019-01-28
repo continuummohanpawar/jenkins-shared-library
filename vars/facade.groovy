b04e8d37b795596ca41a45222a70c8d6b85ce204
continuum-visharad-dhavle <visharad.dhavle@continuum.net>
2019-01-28 21:15:19 +0530

    new way 28

--- vars/facade.groovy
+++ vars/facade.groovy
@@ -1,5 +1,4 @@
 import groovy.json.JsonSlurper
-import org.my.jenkins
 
 def execute()
 {
@@ -19,7 +18,7 @@ def readYamlConfig(String config)
     def _config = readYaml text: config
     println(_config.name)
     l.log("Reading YAML file - done", "ERROR")
-    def lo = new Loggy()
+    def lo = new org.my.jenkins.Loggy()
     lo.log("Reading YAML file LOGGY - done", "ERROR")
 }
 
