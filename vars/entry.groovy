a8dcf183458d79dc2707d436315fa8a299f70e73
continuum-visharad-dhavle <visharad.dhavle@continuum.net>
2019-01-28 14:42:37 +0530

    two containers 02

--- vars/entry.groovy
+++ vars/entry.groovy
@@ -17,7 +17,7 @@ def call(body) {
     /*  */
     stage 'build stage'
     kubernetes.pod('buildpod1').
-        withNewContainer().withName("maven-1").withImage('nginx').and().withNewContainer().withName().withImage('maven').inside{
+        withNewContainer().withName("maven-1").withImage('nginx').and().withNewContainer().withName("ff").withImage('maven').inside{
             sh 'some shell commands that are going to be run inside name2'
         }
 }
