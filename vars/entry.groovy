5b2385f97fab038528fa0bc48ab5b68c071ef97b
continuum-visharad-dhavle <visharad.dhavle@continuum.net>
2019-01-28 21:42:15 +0530

    new way 39

--- vars/entry.groovy
+++ vars/entry.groovy
@@ -5,10 +5,29 @@ def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
-    body()   
-
-    node{
-        sh 'echo Hello World'
-    }   
+    body()
     
+    def label = "mongo-${UUID.randomUUID().toString()}"
+    podTemplate(label: label, yaml: """
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    some-label: some-label-value
+spec:
+  containers:
+  - name: maven
+    image: maven:3.3.9-jdk-8-alpine
+    command: ['cat']
+    tty: true
+  - name: mongo
+    image: mongo
+"""
+  )
+
+    node(label){
+        container('maven') {
+            sh 'echo Hello World'
+        }
+    }
 }
